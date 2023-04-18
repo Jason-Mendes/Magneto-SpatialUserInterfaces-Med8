f4dcab2bc12b8b0a00b195a29b6ecef8455a2a01
Herrelaekker <jacobhlnhtxb16@gmail.com>
2023-04-18 16:50:02 +0200

    Revert ":negative_squared_cross_mark:"
    This reverts commit d7752234de6b302a42f89fbb7823f83d4a4bb293.

--- Magnet_Demo2/Assets/Scenes/Scripts/PlayerController.cs
+++ Magnet_Demo2/Assets/Scenes/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using static Utilities;
 
 public class PlayerController : MonoBehaviour
@@ -42,9 +41,6 @@ public class PlayerController : MonoBehaviour
     }
     void Update() // Resets certain variables every frame
     {
-        if (inputDevices.Count < 2)
-            InitializeInputReader();
-
         GetMoveInput();
         polarity = magInput;
     }
@@ -114,19 +110,5 @@ public class PlayerController : MonoBehaviour
         // Mouse controlled camera movement
         cam.transform.localRotation = Quaternion.Euler(mouseInput.y,0,0);
         transform.localRotation = Quaternion.Euler(0,mouseInput.x,0); 
-    }
-
-    List<InputDevice> inputDevices = new List<InputDevice>();
-
-    void InitializeInputReader()
-    {
-        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller, inputDevices);
-
-        foreach(var inputDevice in inputDevices)
-        {
-            inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-
-            Debug.Log(inputDevice.name + " " + triggerValue);
-        }
     }
 }
